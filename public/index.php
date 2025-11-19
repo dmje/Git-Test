2455c4be181bd6c7aee3e0974e0f04551e5f6013
Mike Ellis <mike@thirty8.co.uk>
2025-11-19 09:53:07 +0000

    Updated the colourful grid thingy

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Hello World - Colorful Grid</title>
+	<title>Hello World - Colourful Grid NOW CHANGED woo</title>
 	<style>
 		* {
 			margin: 0;
